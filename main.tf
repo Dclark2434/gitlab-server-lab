98d186c47021adff6c7bbdec2fa3753365449ed9
Dustin Clark <138034989+Dclark2434@users.noreply.github.com>
2023-09-28 22:12:59 -0500

    refactored code for improved resource ordering

--- main.tf
+++ main.tf
@@ -9,6 +9,7 @@ resource "aws_key_pair" "gitlab_key" {
 
 resource "aws_vpc" "gitlab_vpc" {
   cidr_block = "10.0.0.0/16"
+
   tags = {
     Name = "gitlab_vpc"
   }
@@ -16,17 +17,19 @@ resource "aws_vpc" "gitlab_vpc" {
 
 resource "aws_internet_gateway" "gitlab_igw" {
   vpc_id = aws_vpc.gitlab_vpc.id
-  
+
   tags = {
     Name = "gitlab_igw"
   }
 }
 
+resource "aws_subnet" "gitlab_subnet" {
+  vpc_id     = aws_vpc.gitlab_vpc.id  
+  cidr_block = "10.0.1.0/24"
+}
 
 resource "aws_route_table" "gitlab_route_table" {
   vpc_id = aws_vpc.gitlab_vpc.id
-  
-  
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.gitlab_igw.id
@@ -40,32 +43,6 @@ resource "aws_route_table" "gitlab_route_table" {
 resource "aws_route_table_association" "gitlab_rta" {
   subnet_id      = aws_subnet.gitlab_subnet.id
   route_table_id = aws_route_table.gitlab_route_table.id
-}
-
-resource "aws_subnet" "gitlab_subnet" {
-  vpc_id     = aws_vpc.gitlab_vpc.id  
-  cidr_block = "10.0.1.0/24"
-}
-
-resource "aws_instance" "gitlab_server" {
-  ami           = "ami-02675d30b814d1daa"
-  instance_type = "t2.medium"
-
-  key_name               = aws_key_pair.gitlab_key.key_name
-  vpc_security_group_ids = [aws_security_group.gitlab_sg.id]
-    subnet_id              = aws_subnet.gitlab_subnet.id
-
-  tags = {
-    Name = "GitLab Server"
-  }
-
-  user_data = <<-EOF
-              #!/bin/bash
-              apt-get update
-              apt-get install -y curl openssh-server ca-certificates
-              curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.deb.sh | sudo bash
-              apt-get install -y gitlab-ce
-              EOF
 }
 
 resource "aws_security_group" "gitlab_sg" {
@@ -102,6 +79,27 @@ resource "aws_security_group" "gitlab_sg" {
     cidr_blocks = ["0.0.0.0/0"] # Allow all outbound traffic
   }
 }
+
+resource "aws_instance" "gitlab_server" {
+  ami           = "ami-02675d30b814d1daa"
+  instance_type = "t2.medium"
+  key_name               = aws_key_pair.gitlab_key.key_name
+  vpc_security_group_ids = [aws_security_group.gitlab_sg.id]
+    subnet_id              = aws_subnet.gitlab_subnet.id
+
+  tags = {
+    Name = "GitLab Server"
+  }
+
+  user_data = <<-EOF
+              #!/bin/bash
+              apt-get update
+              apt-get install -y curl openssh-server ca-certificates
+              curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.deb.sh | sudo bash
+              apt-get install -y gitlab-ce
+              EOF
+}
+
 
 resource "aws_eip" "gitlab_eip" {
   instance = aws_instance.gitlab_server.id
